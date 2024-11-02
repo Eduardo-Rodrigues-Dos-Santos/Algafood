1e3fd87726333220acff7ad4c054ee3a3f59de76
Eduardo-Rodrigues-Dos-Santos <Eduardo-Rodrigues-Dos-Santos@proton.me>
2024-11-01 23:38:57 -0300

    add correction to GroupPermissionController status code.

--- src/main/java/org/algaworks/algafood/api/controllers/GroupPermissionController.java
+++ src/main/java/org/algaworks/algafood/api/controllers/GroupPermissionController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.algaworks.algafood.api.mapper.PermissionMapper;
 import org.algaworks.algafood.api.models.PermissionModel;
 import org.algaworks.algafood.core.security.security_annotations.CheckSecurity;
+import org.algaworks.algafood.domain.exceptions.BusinessException;
+import org.algaworks.algafood.domain.exceptions.GroupNotFoundException;
 import org.algaworks.algafood.domain.services.GroupService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,22 +25,34 @@ public class GroupPermissionController {
     @CheckSecurity.User.ConsultGroup
     @GetMapping
     public ResponseEntity<Set<PermissionModel>> findAllPermissions(@PathVariable Long groupId) {
-        Set<PermissionModel> permissions = groupService.findAllPermissions(groupId).stream()
-                .map(permissionMapper::toPermissionModel).collect(Collectors.toSet());
-        return ResponseEntity.ok(permissions);
+        try {
+            Set<PermissionModel> permissions = groupService.findAllPermissions(groupId).stream()
+                    .map(permissionMapper::toPermissionModel).collect(Collectors.toSet());
+            return ResponseEntity.ok(permissions);
+        } catch (GroupNotFoundException e) {
+            throw new BusinessException(e.getMessage());
+        }
     }
 
     @CheckSecurity.User.ManageGroup
     @PutMapping("/{permissionId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void attachPermission(@PathVariable Long groupId, @PathVariable Long permissionId) {
-        groupService.attachPermission(groupId, permissionId);
+        try {
+            groupService.attachPermission(groupId, permissionId);
+        } catch (GroupNotFoundException e) {
+            throw new BusinessException(e.getMessage());
+        }
     }
 
     @CheckSecurity.User.ManageGroup
     @DeleteMapping("/{permissionId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void detachPermission(@PathVariable Long groupId, @PathVariable Long permissionId) {
-        groupService.detachPermission(groupId, permissionId);
+        try {
+            groupService.detachPermission(groupId, permissionId);
+        } catch (BusinessException e) {
+            throw new BusinessException(e.getMessage());
+        }
     }
 }
